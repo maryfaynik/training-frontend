4bf20be72588db1b7e2317011587b904e6df0743
maryfarner18 <maryfarner@gmail.com>
2020-01-05 10:00:49 -0500

    Updates

---
+++ precache-manifest.d553f7729299865640083e3baf724f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8752d14c42f00b5e26f2e7e50f1f3e99",
+    "url": "/training-manager-frontend/index.html"
+  },
+  {
+    "revision": "65c56d336d89832ba292",
+    "url": "/training-manager-frontend/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "4c7c55672d86703bbec2",
+    "url": "/training-manager-frontend/static/css/main.880985b9.chunk.css"
+  },
+  {
+    "revision": "65c56d336d89832ba292",
+    "url": "/training-manager-frontend/static/js/2.dd08a2df.chunk.js"
+  },
+  {
+    "revision": "562b8f59752f4e9722d05cde5e626bfb",
+    "url": "/training-manager-frontend/static/js/2.dd08a2df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c7c55672d86703bbec2",
+    "url": "/training-manager-frontend/static/js/main.17794cff.chunk.js"
+  },
+  {
+    "revision": "5ca7b87108f166c04f54",
+    "url": "/training-manager-frontend/static/js/runtime-main.a1f9d00f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e0dff2ff47a24687c646903b40057ee.js"
+  "/training-manager-frontend/precache-manifest.d553f7729299865640083e3baf724f38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/training-manager-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
